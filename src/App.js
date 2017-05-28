af5da5f7d41442356ac6c0f0116a4f095615b815
Ryan Delaney <rrdelaney@outlook.com>
2017-05-27 17:05:12 -0700

    Wire up to launchpad

--- src/App.js
+++ src/App.js
@@ -1,28 +1,95 @@
 import React from 'react'
-import { gql, graphql } from 'react-apollo'
+import { gql, graphql, compose } from 'react-apollo'
 
-const AllCharacters = gql`
-  query AllCharacters {
-    allPersons {
+const AllUsersQuery = gql`
+  query AllUsers {
+    allUsers {
       id
       name
+      posts {
+        id
+        title
+      }
     }
   }
 `
 
-function App ({ data }) {
-  if (data.loading) return <p>Loading...</p>
+const AllUsers = graphql(AllUsersQuery)
 
-  return (
-    <div>
-      <h2>Characters</h2>
-      <ul>
-        {data.allPersons.map(c =>
-          <li key={c.id}>{c.name}</li>
-        )}
-      </ul>
-    </div>
-  )
+const AddPost = graphql(gql`
+  mutation AddPost ($author: ID!, $title: String!, $content: String!) {
+    addPost(author: $author, title: $title, content: $content) {
+      id
+      title
+    }
+  }
+`)
+
+class App extends React.Component {
+  state = {
+    author: '',
+    title: '',
+    content: ''
+  }
+
+  create = () => {
+    this.props.mutate({
+      variables: this.state,
+      refetchQueries: [
+        { query: AllUsersQuery }
+      ]
+    })
+  }
+
+  update = field => e => {
+    this.setState({
+      [field]: e.target.value
+    })
+  }
+
+  render () {
+    const { data } = this.props
+
+    if (data.loading) return <p>Loading...</p>
+
+    return (
+      <div>
+        <h2>Users</h2>
+        <ul>
+          {data.allUsers.map(c =>
+            <li key={c.id}>
+              ({c.id}) {c.name}
+              <ul>
+                {c.posts.map(p =>
+                  <li key={p.id}>{p.title}</li>
+                )}
+              </ul>
+            </li>
+          )}
+        </ul>
+
+        <div>
+          <h2>Create Post</h2>
+          <div>
+            <label>Author</label>
+            <input onChange={this.update('author')} value={this.state.author} />
+          </div>
+          <div>
+            <label>Title</label>
+            <input onChange={this.update('title')} value={this.state.title} />
+          </div>
+          <div>
+            <label>Content</label>
+            <input onChange={this.update('content')} value={this.state.content} />
+          </div>
+          <button onClick={this.create}>Create</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default graphql(AllCharacters)(App)
+export default compose(
+  AddPost,
+  AllUsers
+)(App)
--- src/configureClient.js
+++ src/configureClient.js
@@ -2,8 +2,9 @@ import { ApolloClient, createNetworkInterface } from 'react-apollo'
 
 export default function configureClient () {
   return new ApolloClient({
+    dataIdFromObject: o => o.id,
     networkInterface: createNetworkInterface({
-      uri: 'https://api.graphcms.com/simple/v1/swapi'
+      uri: 'https://7vkj4v8vj.lp.gql.zone/graphql'
     }),
   })
 }
